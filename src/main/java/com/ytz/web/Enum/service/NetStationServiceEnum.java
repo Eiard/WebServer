0e1ebdb9820b966480bfa7841ccfd275aebe93f9
30671 <3067132996@qq.com>
2022-07-04 20:29:51 +0800

    2022/7/3

---
+++ src/main/java/com/ytz/web/Enum/service/NetStationServiceEnum.java
@@ -0,0 +1,16 @@
+package com.ytz.web.Enum.service;
+
+/**
+ * -*- coding:utf-8 -*-
+ *
+ * @projectName: web
+ * @package: com.ytz.web.Enum.service
+ * @className: NetStationServiceEnum
+ * @author: 30671
+ * @description: TODO :
+ * @date: 2022/7/4
+ * @version: 1.0
+ */
+
+public enum NetStationServiceEnum {
+}
