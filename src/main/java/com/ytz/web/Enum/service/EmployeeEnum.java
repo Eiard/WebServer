68864ed9fd427776cef675d9b3ffad1a7ce65446
30671 <3067132996@qq.com>
2022-07-05 09:19:37 +0800

    2022/7/5

---
+++ src/main/java/com/ytz/web/Enum/service/EmployeeEnum.java
@@ -0,0 +1,16 @@
+package com.ytz.web.Enum.service;
+
+/**
+ * -*- coding:utf-8 -*-
+ *
+ * @projectName: web
+ * @package: com.ytz.web.Enum.service
+ * @className: EmployeeEnum
+ * @author: 30671
+ * @description: TODO : Employee状态码
+ * @date: 2022/7/5
+ * @version: 1.0
+ */
+
+public enum EmployeeEnum {
+}
