139f617a11bb0e8a59f5e2f50a0813ff8fa75784
30671 <3067132996@qq.com>
2022-07-11 22:06:44 +0800

    2022/7/11

---
+++ src/main/java/com/ytz/web/mapper/EmployeePositionMapper.java
@@ -0,0 +1,18 @@
+package com.ytz.web.mapper;
+
+import com.ytz.web.domain.EmployeePosition;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 30671
+* @description 针对表【employee_position】的数据库操作Mapper
+* @createDate 2022-07-11 22:04:57
+* @Entity com.ytz.web.domain.EmployeePosition
+*/
+public interface EmployeePositionMapper extends BaseMapper<EmployeePosition> {
+
+}
+
+
+
+
