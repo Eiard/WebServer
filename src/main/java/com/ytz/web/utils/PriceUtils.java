8bca62fa0111f5fcbc53c901a58566368f1a0081
30671 <3067132996@qq.com>
2022-07-19 16:58:09 +0800

    2022/7/18

--- src/main/java/com/ytz/web/utils/PriceUtils.java
+++ src/main/java/com/ytz/web/utils/PriceUtils.java
@@ -19,20 +19,20 @@ public class PriceUtils {
 
     public static Double getFinalPrice(Double weight, Integer goodType) {
         Double finalPrice = 0.0;
-        if (weight <= 1) {
+        if (weight <= 5) {
             if (goodType == 1) finalPrice = generalCargoBasePrice * weight;
             if (goodType == 2) finalPrice = sensitiveCargoBasePrice * weight;
             if (goodType == 3) finalPrice = foodStuffBasePrice * weight;
             if (goodType == 4) finalPrice = medicinalBasePrice * weight;
             if (goodType == 5) finalPrice = batteryCargoBasePrice * weight;
             if (goodType == 6) finalPrice = documentCargoBasePrice * weight;
-        }else{
-            if (goodType == 1) finalPrice = generalCargoBasePrice + 2*(weight-1);
-            if (goodType == 2) finalPrice = sensitiveCargoBasePrice +3* (weight-1);
-            if (goodType == 3) finalPrice = foodStuffBasePrice + 3*(weight-1);
-            if (goodType == 4) finalPrice = medicinalBasePrice + 3*(weight-1);
-            if (goodType == 5) finalPrice = batteryCargoBasePrice + 2*(weight-1);
-            if (goodType == 6) finalPrice = documentCargoBasePrice + 5*(weight-1);
+        } else {
+            if (goodType == 1) finalPrice = generalCargoBasePrice + 2 * (weight - 1);
+            if (goodType == 2) finalPrice = sensitiveCargoBasePrice + 3 * (weight - 1);
+            if (goodType == 3) finalPrice = foodStuffBasePrice + 3 * (weight - 1);
+            if (goodType == 4) finalPrice = medicinalBasePrice + 3 * (weight - 1);
+            if (goodType == 5) finalPrice = batteryCargoBasePrice + 2 * (weight - 1);
+            if (goodType == 6) finalPrice = documentCargoBasePrice + 5 * (weight - 1);
         }
         return finalPrice;
     }
