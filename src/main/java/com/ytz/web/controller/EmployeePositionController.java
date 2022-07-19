d6ddddd0e1f516ebe7506ed1430ec04ae3b5cda7
30671 <3067132996@qq.com>
2022-07-19 21:25:48 +0800

    2022/7/18

---
+++ src/main/java/com/ytz/web/controller/EmployeePositionController.java
@@ -0,0 +1,21 @@
+package com.ytz.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * -*- coding:utf-8 -*-
+ *
+ * @projectName: web
+ * @package: com.ytz.web.controller
+ * @className: EmployeePositionController
+ * @author: 30671
+ * @description: FIXME : 舍弃(后续选择性实现)
+ * @date: 2022/7/19
+ * @version: 1.0
+ */
+@RestController
+@RequestMapping("/employeePosition")
+public class EmployeePositionController {
+
+}
